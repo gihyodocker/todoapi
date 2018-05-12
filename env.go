fdb679d28e836aa5994a16f4bf3c9fbe21b8b0a8
stormcat24 <stormcat24@stormcat.io>
2018-05-13 04:24:51 +0900

    add env comment

--- env.go
+++ env.go
@@ -5,6 +5,7 @@ import (
 	"os"
 )
 
+// 必要な環境変数を格納するための構造体
 type Env struct {
 	Bind      string
 	MasterURL string
@@ -15,19 +16,19 @@ func CreateEnv() (*Env, error) {
 
 	env := Env{}
 
-	bind := os.Getenv("TODO_BIND")
+	bind := os.Getenv("TODO_BIND") // APIをListenするポート設定
 	if bind == "" {
 		env.Bind = ":8080"
 	}
 	env.Bind = bind
 
-	masterURL := os.Getenv("TODO_MASTER_URL")
+	masterURL := os.Getenv("TODO_MASTER_URL") // MySQL Masterへの接続情報
 	if masterURL == "" {
 		return nil, errors.New("TODO_MASTER_URL is not specified")
 	}
 	env.MasterURL = masterURL
 
-	slaveURL := os.Getenv("TODO_SLAVE_URL")
+	slaveURL := os.Getenv("TODO_SLAVE_URL") // MySQL Slaveへの接続情報
 	if slaveURL == "" {
 		return nil, errors.New("TODO_SLAVE_URL is not specified")
 	}
